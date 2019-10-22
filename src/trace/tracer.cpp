4d8e60cb7bea3c0ccde229cf6f2d912428b5a11c
Frank Qin <qzh@cs.washington.edu>
2019-10-22 00:10:30 -0700

    Check linux version for PTRACE_O_EXITKILL

--- src/trace/tracer.cpp
+++ src/trace/tracer.cpp
@@ -3,6 +3,7 @@
 #include <exceptions.h>
 
 #include <cassert>
+#include <linux/version.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
@@ -25,8 +26,13 @@ void run_with_callbacks(const std::vector<std::string> &args, const std::functio
 		// child process fails to start and has printed the error
 		throw ChildExitException{WEXITSTATUS(wstatus)};
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,11,0)
 	// kill target if GravelBox is killed
 	check(::ptrace(PTRACE_SETOPTIONS, child, nullptr, PTRACE_O_EXITKILL));
+#else
+	// TODO(qzh): find a better solution
+#warning Linux kernel version < 3.11, PTRACE_I_KILLEXIT is disabled
+#endif
 	while (true) {
 		check(::ptrace(PTRACE_SYSCALL, child, nullptr, 0));
 		// syscall entry
