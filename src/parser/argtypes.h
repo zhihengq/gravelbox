55c8c498ff71857aa97602e425ef7c7f8bb3fdbd
Frank Qin <qzh@cs.washington.edu>
2019-10-25 14:46:45 -0700

    Add id to ArgType's

--- src/parser/argtypes.h
+++ src/parser/argtypes.h
@@ -1,8 +1,9 @@
 #ifndef ARGTYPES_H_
 #define ARGTYPES_H_
 
-#include <cstdint>
+#include <sys/types.h>
 
+#include <cstdint>
 #include <ostream>
 
 namespace GravelBox {
@@ -28,10 +29,34 @@ class Arg {
  */
 class ArgType {
   public:
+	/**
+	 * Instantiate an `Arg` with an argument register value.
+	 *
+	 * @param arg the argument register value.
+	 * @return Arg the argument to be printed.
+	 */
 	Arg operator()(uint64_t arg) const noexcept { return {this, arg}; }
+
+	/**
+	 * Cleanup the `ArgType` object.
+	 */
 	virtual ~ArgType() {}
 
+	/**
+	 * Print a value of this argument type.
+	 *
+	 * @param os the output stream.
+	 * @param value an argument register value of this type.
+	 */
 	virtual void write(std::ostream &os, uint64_t value) const = 0;
+
+	/**
+	 * Return the string representation of the type.
+	 * Used in the config file.
+	 *
+	 * @return the string id.
+	 */
+	virtual std::string id() const noexcept = 0;
 };
 
 /**
@@ -51,6 +76,7 @@ inline std::ostream &operator<<(std::ostream &os, const Arg &arg) {
  */
 struct UnknownType : public ArgType {
 	void write(std::ostream &os, uint64_t value) const override;
+	std::string id() const noexcept override;
 };
 
 /**
@@ -58,6 +84,7 @@ struct UnknownType : public ArgType {
  */
 struct SIntType : public ArgType {
 	void write(std::ostream &os, uint64_t value) const override;
+	std::string id() const noexcept override;
 };
 
 /**
@@ -65,6 +92,7 @@ struct SIntType : public ArgType {
  */
 struct UIntType : public ArgType {
 	void write(std::ostream &os, uint64_t value) const override;
+	std::string id() const noexcept override;
 };
 
 /**
@@ -73,6 +101,29 @@ struct UIntType : public ArgType {
  */
 struct PtrType : public ArgType {
 	void write(std::ostream &os, uint64_t value) const override;
+	std::string id() const noexcept override;
+};
+
+/**
+ * String type.
+ * Read target memory and print the null-terminated string.
+ */
+struct StrType : public ArgType {
+	void write(std::ostream &os, uint64_t value) const override;
+	std::string id() const noexcept override;
+
+	/**
+	 * Set the target pid.
+	 * This allows the type to read target's memory.
+	 *
+	 * @param pid the pid of the target.
+	 */
+	void setpid(pid_t pid);
+
+	/**
+	 * Cleanup the file descriptor used to read the target's memory.
+	 */
+	~StrType() override;
 };
 
 }  // namespace GravelBox
