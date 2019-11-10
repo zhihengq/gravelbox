12b406e65d58366382868d16a3a0dd6273d30619
Frank Qin <qzh@cs.washington.edu>
2019-11-10 15:48:31 -0800

    Fix #1
    Filter only system calls.

--- src/trace/tracer.cpp
+++ src/trace/tracer.cpp
@@ -2,12 +2,13 @@
 #include <utils.h>
 #include <exceptions.h>
 
-#include <cassert>
 #include <linux/version.h>
+#include <signal.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
 
+#include <cassert>
 #include <functional>
 
 namespace GravelBox {
@@ -21,8 +22,11 @@ void run_with_callbacks(
 	const std::function<void(pid_t)> &pid_callback,
 	const std::function<bool(user_regs_struct)> &syscall_callback) {
 	// spawn child
-	pid_t child = Utils::spawn(
-		args, []() { check(::ptrace(PTRACE_TRACEME, 0, nullptr, nullptr)); });
+	pid_t child = Utils::spawn(args, []() {
+		check(::ptrace(PTRACE_TRACEME, 0, nullptr, nullptr));
+		if (::raise(SIGSTOP) != 0)
+			Utils::throw_system_error();
+	});
 	pid_callback(child);
 
 	// wait for child process to be ready for trace
@@ -32,40 +36,43 @@ void run_with_callbacks(
 		// child process fails to start and has printed the error
 		throw ChildExitException{WEXITSTATUS(wstatus)};
 
+	uint64_t options = PTRACE_O_TRACESYSGOOD;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 11, 0)
 	// kill target if GravelBox is killed
-	check(::ptrace(PTRACE_SETOPTIONS, child, nullptr, PTRACE_O_EXITKILL));
+	options |= PTRACE_O_EXITKILL;
 #else
-		// TODO(qzh): find a better solution
+	// TODO(qzh): find a better solution
 #warning Linux kernel version < 3.11, PTRACE_I_KILLEXIT is disabled
 #endif
+	check(::ptrace(PTRACE_SETOPTIONS, child, nullptr, options));
 	while (true) {
 		check(::ptrace(PTRACE_SYSCALL, child, nullptr, 0));
 		// syscall entry
 		check(::waitpid(child, &wstatus, 0));
 		if (WIFEXITED(wstatus)) {
 			break;
-		}
-
-		user_regs_struct regs;
-		check(::ptrace(PTRACE_GETREGS, child, nullptr, &regs));
-		bool deny = !syscall_callback(regs);
-		if (deny) {
-			regs.orig_rax = -1;
-			check(::ptrace(PTRACE_SETREGS, child, nullptr, &regs));
-		}
-
-		check(::ptrace(PTRACE_SYSCALL, child, nullptr, 0));
-		// syscall exit
-		check(::waitpid(child, &wstatus, 0));
-		if (WIFEXITED(wstatus)) {
-			break;
-		}
-
-		if (deny) {
+		} else if (WIFSTOPPED(wstatus)
+				   && WSTOPSIG(wstatus) == (SIGTRAP | 0x80)) {
+			user_regs_struct regs;
 			check(::ptrace(PTRACE_GETREGS, child, nullptr, &regs));
-			regs.rax = -EPERM;
-			check(::ptrace(PTRACE_SETREGS, child, nullptr, &regs));
+			bool deny = !syscall_callback(regs);
+			if (deny) {
+				regs.orig_rax = -1;
+				check(::ptrace(PTRACE_SETREGS, child, nullptr, &regs));
+			}
+
+			check(::ptrace(PTRACE_SYSCALL, child, nullptr, 0));
+			// syscall exit
+			check(::waitpid(child, &wstatus, 0));
+			if (WIFEXITED(wstatus)) {
+				break;
+			}
+
+			if (deny) {
+				check(::ptrace(PTRACE_GETREGS, child, nullptr, &regs));
+				regs.rax = -EPERM;
+				check(::ptrace(PTRACE_SETREGS, child, nullptr, &regs));
+			}
 		}
 	}
 }
