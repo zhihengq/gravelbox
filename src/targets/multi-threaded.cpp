7419903faf24f644b989613c7d0cf9609e07ce06
Frank Qin <qzh@cs.washington.edu>
2019-11-10 22:46:12 -0800

    Fix multi-threaded target

--- src/targets/multi-threaded.cpp
+++ src/targets/multi-threaded.cpp
@@ -11,6 +11,11 @@ int main(int argc, char **argv) {
 	for (int i = 1; i < argc; i++) {
 		threads.emplace_back([&m, f = argv[i]]() {
 			std::ifstream is(f);
+			if (!is.is_open()) {
+				std::scoped_lock lock(m);
+				std::cerr << "Error: Cannot open " << f << std::endl;
+				return;
+			}
 			std::string s;
 			std::getline(is, s);
 			std::scoped_lock lock(m);
