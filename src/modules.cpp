4c5ff9fa978fe331b0974b91967774d462cff12a
Frank Qin <qzh@cs.washington.edu>
2019-12-02 13:47:04 -0800

    Allow command line override of the pinentry program

--- src/modules.cpp
+++ src/modules.cpp
@@ -5,6 +5,7 @@
 #include <logger/logger.h>
 #include <ui/pinentry_ui.h>
 
+#include <memory>
 #include <vector>
 
 #include <boost/program_options.hpp>
@@ -14,24 +15,31 @@ namespace GravelBox {
 int run(const boost::program_options::variables_map &vm) {
 	auto config = std::make_unique<GravelBox::FileConfig>(
 		vm.at("config").as<std::string>());
+
+	std::unique_ptr<GravelBox::PinentryUI> ui;
+	if (vm.count("pinentry") > 0)
+		ui = std::make_unique<GravelBox::PinentryUI>(
+			vm.at("pinentry").as<std::string>());
 	if (vm.at("no-signature").as<bool>()) {
 		config->dismiss_signature();
 	} else {
 		constexpr auto message = "Enter the configuration file signing key.";
 		constexpr auto prompt = "key: ";
-		GravelBox::PinentryUI valid_ui("pinentry");
-		PinentryUI::Password key = valid_ui.ask_password(message, prompt, "");
+		if (ui == nullptr)
+			ui = std::make_unique<GravelBox::PinentryUI>("pinentry");
+		PinentryUI::Password key = ui->ask_password(message, prompt, "");
 		if (!key)
 			return EXIT_FAILURE;
 		while (!config->verify_signature(std::move(key.password))) {
-			key = valid_ui.ask_password(
+			key = ui->ask_password(
 				message, prompt,
 				"Incorrect key, or the configuration has been changed.");
 			if (!key)
 				return EXIT_FAILURE;
 		}
 	}
-	auto ui = std::make_unique<GravelBox::PinentryUI>(config->pinentry());
+	if (vm.count("pinentry") == 0)
+		auto ui = std::make_unique<GravelBox::PinentryUI>(config->pinentry());
 	auto parser = std::make_unique<GravelBox::Parser>(config->syscalldef());
 	auto logger = std::make_unique<GravelBox::Logger>();
 	GravelBox::Tracer tracer(std::move(parser), std::move(config),
